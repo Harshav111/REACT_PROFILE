6391ca8556e44b141cd5aa6ae0588279f449efaf
Harshav111 <sj.harsha2382003@gmail.com>
2024-09-14 12:22:45 +0530

    Updates

---
+++ _next/static/ht7F1SQyF_S0pGi0S_eNH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/550-eefb2124a82cc7d6.js","static/chunks/pages/index-3f234b2f6bc33e7f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
